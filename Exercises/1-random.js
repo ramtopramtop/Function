d8355b662d521cc42f78d0e9190cdecd44997e3b
ramtopramtop <ramtopramtop@rambler.ru>
2024-10-15 17:13:19 +0300

    Task 1&2 lint fixes

--- Exercises/1-random.js
+++ Exercises/1-random.js
@@ -5,7 +5,7 @@ const random = (min, max) => {
     max = min;
     min = 0;
   }
-  return (min + Math.floor((Math.random() * (max - min + 1)) - 0,5));
+  return (min + Math.floor((Math.random() * (max - min + 1)) - 0, 5));
 };
 
 module.exports = { random };
--- Exercises/2-key.js
+++ Exercises/2-key.js
@@ -3,7 +3,7 @@
 const generateKey = (length, possible) => {
   const strLen = possible.length;
   let string = '';
-  
+
   for (let i = 0; i < length; i++) {
     string += possible[Math.floor((Math.random() * (strLen)) - 0, 5)];
   }
