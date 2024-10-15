4d6328ffceefa2ce6806c96942b17053744ccc29
ramtopramtop <ramtopramtop@rambler.ru>
2024-10-15 18:06:22 +0300

    Task 3 working wariant

--- Exercises/3-ip.js
+++ Exercises/3-ip.js
@@ -1,11 +1,14 @@
 'use strict';
 
 const ipToInt = (ip = '127.0.0.1') => {
-  // Parse ip address as string, for example '10.0.0.1'
-  // to ['10', '0', '0', '1'] to [10, 0, 0, 1]
-  // and convert to Number value 167772161 with bitwise shift
-  // (10 << 8 << 8 << 8) + (0 << 8 << 8) + (0 << 8) + 1 === 167772161
-  // Use Array.prototype.reduce of for loop
+  let ipArr = ip.split('.').map(Number);
+  let shift = 3;
+  let shiftedIpArr = [];
+  for (let value of ipArr) {
+    shiftedIpArr.push(value << shift);
+    shift = shift--;
+  }
+  return shiftedIpArr.reduce((sum, x) => sum = sum + x, 0);
 };
-
+//console.log(ipToInt());
 module.exports = { ipToInt };
