f3dd570b6743c607435106a6a610b762719da74c
ramtopramtop <ramtopramtop@rambler.ru>
2024-10-16 17:03:05 +0300

    4 task done

--- Exercises/4-methods.js
+++ Exercises/4-methods.js
@@ -1,21 +1,13 @@
 'use strict';
 
-const methods = iface => {
-  // Introspect all properties of iface object and
-  // extract function names and number of arguments
-  // For example: {
-  //   m1: x => [x],
-  //   m2: function (x, y) {
-  //     return [x, y];
-  //   },
-  //   m3(x, y, z) {
-  //     return [x, y, z];
-  //   }
-  // will return: [
-  //   ['m1', 1],
-  //   ['m2', 2],
-  //   ['m3', 3]
-  // ]
+const methods = (iface) => {
+  const intro = [];
+  for (const field in iface) {
+    if (typeof iface[field] === 'function') {
+      intro.push([iface[field].name, iface[field].length]);
+    }
+  }
+  return intro;
 };
 
 module.exports = { methods };
