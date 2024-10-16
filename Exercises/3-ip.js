75e670c1993301d408595c6bbf04def439101f96
ramtopramtop <ramtopramtop@rambler.ru>
2024-10-16 15:20:10 +0300

    Task 3 lint correct

--- Exercises/3-ip.js
+++ Exercises/3-ip.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const ipToInt = (ip = '127.0.0.1') => {
-  return ip.split('.').reduce((sum, x) => sum = (sum << 8) + parseInt(x), 0);
-};
+const ipToInt = (ip = '127.0.0.1') =>
+  ip.split('.').reduce((sum, x) => sum = (sum << 8) + parseInt(x), 0);
+
 
 module.exports = { ipToInt };
